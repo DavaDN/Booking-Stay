98dc2519ffe8a8cc71c2f129d4c252b69390bd40
DavaDN <ayujas321@gmail.com>
2025-08-03 16:44:38 +0700

    Booking-stay

--- routes/web.php
+++ routes/web.php
@@ -6,20 +6,38 @@ use App\Http\Controllers\RoomTypeController;
 use App\Http\Controllers\RoomController;
 use App\Http\Controllers\BookingController;
 use App\Http\Controllers\TransactionController;
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\Auth\RegisteredUserController;
 
+// Welcome Page
 Route::get('/', function () {
     return view('welcome');
 });
 
+// Login & Register (Breeze)
+Route::get('/login', [AuthenticatedSessionController::class, 'create'])
+    ->middleware('guest')->name('login');
+Route::post('/login', [AuthenticatedSessionController::class, 'store'])
+    ->middleware('guest');
+
+Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])
+    ->middleware('auth');
+
+Route::get('/register', [RegisteredUserController::class, 'create'])
+    ->middleware('guest')->name('register');
+Route::post('/register', [RegisteredUserController::class, 'store'])
+    ->middleware('guest');
+
+// Dashboard + Management
 Route::middleware(['auth'])->group(function () {
     Route::get('/dashboard', fn() => view('dashboard'))->name('dashboard');
 
-    // Data Master
+    // Master Data
     Route::resource('floors', FloorController::class)->only(['index', 'store', 'update', 'destroy']);
     Route::resource('room-types', RoomTypeController::class)->only(['index', 'store', 'update', 'destroy']);
     Route::resource('rooms', RoomController::class)->only(['index', 'store', 'update', 'destroy']);
 
-    // Booking & Transaksi
+    // Booking & Transaction View
     Route::get('/bookings', [BookingController::class, 'index'])->name('bookings.index');
     Route::get('/transactions', [TransactionController::class, 'index'])->name('transactions.index');
 });
