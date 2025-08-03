a742ffedb69d6e9bfd9a7081bb67637763c4199a
DavaDN <ayujas321@gmail.com>
2025-08-03 16:03:30 +0700

    Booking-stay

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -4,11 +4,23 @@
 namespace App\Models;
 
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Notifications\Notifiable;
 
 class Customer extends Authenticatable
 {
-    protected $fillable = ['name', 'email', 'password'];
-    protected $hidden = ['password'];
+    use HasApiTokens, Notifiable;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    protected $hidden = [
+        'password',
+    ];
+
     public function bookings()
     {
         return $this->hasMany(Booking::class);
